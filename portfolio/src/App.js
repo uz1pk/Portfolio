2b8ec05de695539b467bf159fb3ec2cffe470622
uz1pk <usmanozaheer@gmail.com>
2022-08-01 17:36:48 -0400

    init

---
+++ portfolio/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>
+          Learn React
+        </h1>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
