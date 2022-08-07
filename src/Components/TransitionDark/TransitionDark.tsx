7babdf3f5a7db5733be7ba73eb81bb21922cca94
uz1pk <usmanozaheer@gmail.com>
2022-08-07 13:33:31 -0400

    real resume uploaded

--- src/Components/TransitionDark/TransitionDark.tsx
+++ src/Components/TransitionDark/TransitionDark.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./TransitionDark.css";
 
 function TransitionDark() {
--- src/Components/TransitionLight/TransitionLight.tsx
+++ src/Components/TransitionLight/TransitionLight.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./TransitionLight.css";
 
 function TransitionLight() {
