fa6ea9b7b2f140fcaa714f0744230cfed9a94b0f
uz1pk <usmanozaheer@gmail.com>
2023-01-31 21:54:58 -0500

    typo fix

--- src/Components/Projects/Project3/project3.tsx
+++ src/Components/Projects/Project3/project3.tsx
@@ -35,7 +35,7 @@ function Project1() {
 
                     <p className="proj-tech-stack proj-body-text">
                         <b>
-                        Golang · React · SCSS · Docker
+                        Golang · React · Sass · Docker
                         </b>
                     </p>
 
